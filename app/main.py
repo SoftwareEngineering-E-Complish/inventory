26438edc1365bce4f25fd15156599a19eaa8fc2d
Georgios <georgios-rafail.daskalopoulos@uzh.ch>
2024-03-14 09:05:05 +0100

    Remove useless API endpoints

--- app/main.py
+++ app/main.py
@@ -10,19 +10,10 @@ from fastapi import HTTPException, Depends
 from decimal import Decimal 
 
 app = FastAPI()
-@app.get("/schema/example")
-def get_schema():
-    return {'price': {'name': 'prie', 'type': 'int', 'description': 'Price of the property'}}
 
 @app.get("/schema/propertyQuery")
 def get_schema():
-    #return Property.schema_json(indent=4)
     return PropertyQuery.model_json_schema(by_alias=True)
-
-@app.get("/schema/sampleType")
-def get_schema():
-    #return Property.schema_json(indent=4)
-    return QueryBasic.model_json_schema(by_alias=True)
 
 @app.get("/properties/", response_model=List[Property])
 def fetch_all_properties():
@@ -35,38 +26,13 @@ def fetch_property_by_key(property_id: str):
         raise HTTPException(status_code=404, detail="Item not found")
     return item
 
-def query_params_to_property(title: Optional[str] = None,
-                             description: Optional[str] = None,
-                             price: Optional[Decimal] = None,
-                             location: Optional[str] = None,
-                             bedrooms: Optional[int] = None,
-                             bathrooms: Optional[int] = None,
-                             square_meters: Optional[int] = None,
-                             year_built: Optional[int] = None,
-                             property_type: Optional[PropertyType] = None,
-                             done: Optional[bool] = None) -> Property:
-    return Property(title=title,
-                    description=description,
-                    price=price,
-                    location=location,
-                    bedrooms=bedrooms,
-                    bathrooms=bathrooms,
-                    square_meters=square_meters,
-                    year_built=year_built,
-                    property_type=property_type,
-                    done=done)
-
-@app.get("/queryPropertiesByAttributes", response_model=List[Property], description="Fetch all the Properties matching the fields provided as parameters")
-def fetch_properties_by_attributes(exampleProperty: Property = Depends(query_params_to_property)):
-    return get_properties_by_attributes(exampleProperty)
-
 
 @app.get("/queryProperties", response_model=List[Property], description="Query the properties based on the fields provided")
 def query_properties(query: PropertyQuery = Depends(PropertyQuery)):
     return query_properties_by_attributes(query)
 
 @app.post("/properties/", response_model=Property)
-def create_prop(property: Property):
+def create_property_listing(property: Property):
     created_item = create_property_autoKey(property)
     return created_item
 
--- app/models/property_query.py
+++ app/models/property_query.py
@@ -1,6 +1,6 @@
 import json, jsonref
 from pydantic import BaseModel, Field
-from typing import Optional, List, Any
+from typing import Optional, List
 from .property import PropertyType, SwissCities
 
 class PropertyQuery(BaseModel):
@@ -17,13 +17,6 @@ class PropertyQuery(BaseModel):
     property_type: Optional[PropertyType] = Field(None, description="Type of the property")
     location: Optional[SwissCities] = Field(None, description="Location of the property, only specific Swiss cities are allowed")
 
-"""
-class Parameter:
-    def __init__(self, name, type, description):
-        self.name = name
-        self.type = type
-        self.description = description
-"""
 
 class QueryBasic(BaseModel):
     sample_int: int = Field(None, description="Sample int")
@@ -31,18 +24,3 @@ class QueryBasic(BaseModel):
     sample_bool: bool 
     sample_enum: PropertyType
     sample_list: List[str]
-
-
-"""
-class QueryBasic(BaseModel):
-    price_min: int = Field(description="Minimum price of the property")
-    price_max: int = Field(description="Maximum price of the property")
-    bedrooms_min: int = Field(ge=1, description="Minimum number of bedrooms in the property. Must be at least 1.")
-    bedrooms_max: int = Field(le=10, description="Maximum number of bedrooms in the property. Must be at most 10.")
-    bathroom_min: int = Field(description="Minimum number of bathrooms in the property")
-    bathroom_max: int = Field(description="Maximum number of bathrooms in the property")
-    square_meters_min: int = Field(description="Minimum square meters of the property")
-    square_meters_max: int = Field(description="Maximum square meters of the property")
-    year_built_from: int = Field(description="Minimum year the property was built")
-    year_built_to: int = Field(description="Maximum year the property was built")
-    property_type: PropertyType = Field(description="Type of the property") """
