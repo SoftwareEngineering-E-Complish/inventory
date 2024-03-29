adb723785528c763f18f660fa99ec28c84a457d2
Georgios <georgios-rafail.daskalopoulos@uzh.ch>
2024-03-29 20:34:12 +0100

    Purge dynamo container and dynamo setup

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # For example: python manage.py migrate
 
 # Start the service
-python dynamo_setup/create_table.py
-python dynamo_setup/insert_test_data.py
+sleep 5 # wait for postgres to actually start, otherwise the script will fail
+python postgres_setup/create_schema.py
+python postgres_setup/insert_test_data.py
 uvicorn app.main:app --host 0.0.0.0 --port 80 --reload
