f285dec7792a63e8201e1598db5371c083e5ab89
Georgios <georgios-rafail.daskalopoulos@uzh.ch>
2024-04-26 12:02:52 +0200

    Add test coverage for update functionality

--- tests/property_relation_service_test.py
+++ tests/property_relation_service_test.py
@@ -87,3 +87,26 @@ class TestPropertyService(unittest.TestCase):
         self.assertEqual(count, 2)
         self.assertEqual(result[0].propertyId, 1)
         self.assertEqual(result[1].propertyId, 2)
+
+    def test_update_property(self):
+        # Arrange
+        mock_session = create_autospec(Session, instance=True)
+        property = Property(propertyId=1, price=100000, bedrooms=2, bathrooms=2, square_meters=30, 
+                            year_built=2000, owner="user1")
+        mock_session.execute.return_value.scalars.return_value.first.return_value = property
+        updated_property = Property(propertyId=2, price=200000, bedrooms=3, owner="user2")
+
+        # Act
+        propertyService = PropertyService()
+        propertyService.set_session(mock_session).update_property(1, updated_property)
+
+        # Assert
+        mock_session.commit.assert_called_once()
+        mock_session.refresh.assert_called_once_with(property)
+        self.assertEqual(property.propertyId, 1)
+        #Assert key is ignored
+        self.assertEqual(property.price, 200000)
+        #Assert user is ignored
+        self.assertEqual(property.owner, "user1")
+        #Assert other fields are updated
+        self.assertEqual(property.bedrooms, 3)
